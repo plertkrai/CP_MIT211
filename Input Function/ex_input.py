985aeedc1486b19eaf2e31914f521ecc98d0bbcb
samPuriwat <puriwat.l@rmutsv.ac.th>
2022-02-04 15:50:31 +0700

    Lab 6 and input function

---
+++ Input Function/ex_input.py
@@ -0,0 +1,22 @@
+# Input Function
+
+# input()
+
+# input str
+print("What's your name: ")
+name = input()
+print(f'Hello, {name}')
+
+# cast str to int
+age = int(input('How old are you: '))
+print(f'{name} is {age} years old.')
+
+# cast str to float
+weight = float(input('How much your weight: '))
+print(f'your, weigth is: {weight}')
+
+
+
+print(name, type(name))
+print(age, type(age))
+print(weight, type(weight))
